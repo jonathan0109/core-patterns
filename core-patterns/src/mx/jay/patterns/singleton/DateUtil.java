a975d250e1df3e427f968a01b8deab30a7bb8bb1
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-02-12 13:39:52 -0600

    singleton pattern - eager initialization

--- core-patterns/src/mx/jay/patterns/singleton/DateUtil.java
+++ core-patterns/src/mx/jay/patterns/singleton/DateUtil.java
@@ -2,18 +2,13 @@ package mx.jay.patterns.singleton;
 
 public class DateUtil {
 	
-	private static DateUtil instance;
+	private static DateUtil instance = new DateUtil();
 
 	private DateUtil() {
 		
 	}
 	
 	public static DateUtil getInstance() {
-		
-		if (instance == null) {
-			
-			instance = new DateUtil();
-		}
 		
 		return instance;
 	}
