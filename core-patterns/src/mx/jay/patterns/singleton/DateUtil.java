3de425434fe4af5ef474074ebd3234d05ff0af01
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-02-12 13:27:17 -0600

    project skeleton, singleton pattern

---
+++ core-patterns/src/mx/jay/patterns/singleton/DateUtil.java
@@ -0,0 +1,20 @@
+package mx.jay.patterns.singleton;
+
+public class DateUtil {
+	
+	private static DateUtil instance;
+
+	private DateUtil() {
+		
+	}
+	
+	public static DateUtil getInstance() {
+		
+		if (instance == null) {
+			
+			instance = new DateUtil();
+		}
+		
+		return instance;
+	}
+}
